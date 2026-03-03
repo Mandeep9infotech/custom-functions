f210a2bd2e3642f76aecb4f79d5a3e0e18a30fc9
Mandeep9infotech <mandeep9infotech@gmail.com>
2026-03-03 15:14:43 +0530

    Fix import alias for production build

--- app/routes/app.debug.tsx
+++ app/routes/app.debug.tsx
@@ -1,6 +1,6 @@
 import type { LoaderFunctionArgs } from "@remix-run/node";
 import { json } from "@remix-run/node";
-import prisma from "~/db.server";
+import prisma from "../db.server";
 
 export const loader = async ({ request }: LoaderFunctionArgs) => {
   const sessions = await prisma.session.findMany();
--- app/routes/app.setup.tsx
+++ app/routes/app.setup.tsx
@@ -1,6 +1,6 @@
 import type { LoaderFunctionArgs } from "@remix-run/node";
 import { json } from "@remix-run/node";
-import { authenticate } from "~/shopify.server";
+import { authenticate } from "../shopify.server";
 
 export const loader = async ({ request }: LoaderFunctionArgs) => {
   const { admin } = await authenticate.admin(request);
